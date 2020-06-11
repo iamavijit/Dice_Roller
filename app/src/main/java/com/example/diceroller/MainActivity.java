567e90ba76366a86e72889d231e47bf37c6f9a28
Avijit <iamavijit.h@gmail.com>
2020-06-11 17:53:13 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.diceroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private ImageView imageView;
+    private Random random = new Random();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.imageView);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rollMyDice();
+            }
+        });
+    }
+    private void rollMyDice(){
+        int myRandomNumber = random.nextInt(6)+1;
+        switch (myRandomNumber){
+            case 1:
+                imageView.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                imageView.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                imageView.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                imageView.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                imageView.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                imageView.setImageResource(R.drawable.dice6);
+        }
+    }
+}
